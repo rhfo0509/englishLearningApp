6ee67097c5454a50659804d06df8d2241ca6ca15
김응서 <ungnam0509@gmail.com>
2024-08-07 12:14:13 +0900

    feat: splashScreen 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 
 import Root from './navigation/Root';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
