09a935236c2c8bc51f0c909bf020bb9544f94158
김응서 <ungnam0509@gmail.com>
2024-08-08 18:00:20 +0900

    feat: 전역 데이터 관리를 위한 context api 세팅

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import SplashScreen from 'react-native-splash-screen';
-
+import {DataContextProvider} from './contexts/DataContext';
 import Root from './navigation/Root';
 
 const App = () => {
@@ -14,7 +14,9 @@ const App = () => {
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
-        <Root />
+        <DataContextProvider>
+          <Root />
+        </DataContextProvider>
       </NavigationContainer>
     </GestureHandlerRootView>
   );
---
+++ front/src/services/data.service.ts
@@ -0,0 +1,75 @@
+import storage from '@react-native-firebase/storage';
+import RNFS from 'react-native-fs';
+
+const getJSONFromFirebase = async (path: string) => {
+  try {
+    const storageRef = storage().ref(path);
+    const url = await storageRef.getDownloadURL();
+    const response = await fetch(url);
+    const json = await response.json();
+
+    return json;
+  } catch (error) {
+    console.error('Error listing files from Firebase', error);
+    return [];
+  }
+};
+
+export async function checkAndUpdateJSON() {
+  const remoteJSONPaths = ['data.json', 'learning/data.json'];
+  const data = {category: null, chapter: null};
+
+  for (const remoteJSONPath of remoteJSONPaths) {
+    const remoteDirPath = remoteJSONPath.substring(
+      0,
+      remoteJSONPath.lastIndexOf('/'),
+    );
+
+    const localJSONPath = `${RNFS.DocumentDirectoryPath}/${remoteJSONPath}`;
+    const localDirPath = `${RNFS.DocumentDirectoryPath}/${remoteDirPath}`;
+
+    try {
+      let localVersion: string | null = null;
+
+      if (await RNFS.exists(localJSONPath)) {
+        const localJSON = JSON.parse(
+          await RNFS.readFile(localJSONPath, 'utf8'),
+        );
+        localVersion = localJSON.version;
+      }
+
+      const remoteJSON = await getJSONFromFirebase(remoteJSONPath);
+      if (!remoteJSON) {
+        throw new Error(`Failed to fetch remote data from ${remoteJSONPath}`);
+      }
+
+      const remoteVersion = remoteJSON.version;
+
+      // Update local file if versions differ
+      if (localVersion !== remoteVersion) {
+        if (!(await RNFS.exists(localDirPath))) {
+          await RNFS.mkdir(localDirPath);
+        }
+        await RNFS.writeFile(
+          localJSONPath,
+          JSON.stringify(remoteJSON, null, 2),
+          'utf8',
+        );
+        remoteJSONPath === 'data.json'
+          ? (data.category = remoteJSON.data)
+          : (data.chapter = remoteJSON.data);
+      } else {
+        const localJSON = JSON.parse(
+          await RNFS.readFile(localJSONPath, 'utf8'),
+        );
+        remoteJSONPath === 'data.json'
+          ? (data.category = localJSON.data)
+          : (data.chapter = localJSON.data);
+      }
+    } catch (error) {
+      console.error(`Error handling JSON file at ${remoteJSONPath}:`, error);
+    }
+  }
+
+  return data;
+}
