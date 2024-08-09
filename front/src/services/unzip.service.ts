060eb2567c9d699dc866a2549d4ae9414e5b5518
김응서 <ungnam0509@gmail.com>
2024-08-09 17:45:20 +0900

    chore: 파일 압축을 위한 라이브러리 세팅

---
+++ front/src/services/unzip.service.ts
@@ -0,0 +1,7 @@
+import storage from '@react-native-firebase/storage';
+import RNFS from 'react-native-fs';
+import {unzip} from 'react-native-zip-archive';
+
+export const unzipFile = async (remotePath: string, localPath: string) => {
+  // TODO: 파이어베이스 스토리지에서 압축 파일을 다운받아서 로컬에서 풀기
+};
