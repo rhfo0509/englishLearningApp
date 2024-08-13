a1a3d2246e9a0d9e49c5db29455a9948ef689e10
김응서 <ungnam0509@gmail.com>
2024-08-13 12:34:04 +0900

    design: 영어 문장 위치 및 색상 조정

--- front/src/screens/sentences/LessonScreen.tsx
+++ front/src/screens/sentences/LessonScreen.tsx
@@ -367,8 +367,7 @@ const styles = StyleSheet.create({
   english: {
     fontSize: 28,
     fontWeight: '600',
-    color: '#f6efbe',
-    marginBottom: 32,
+    color: '#ffd400',
   },
   translation: {
     fontSize: 20,
