d5be07537a035538e598289266330d9b998580d0
김응서 <ungnam0509@gmail.com>
2024-08-14 16:22:09 +0900

    refactor: service 함수 리팩토링

--- front/src/services/data.service.ts
+++ front/src/services/data.service.ts
@@ -1,73 +1,149 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import storage from '@react-native-firebase/storage';
+import {
+  checkJSONVersion,
+  readLocalJSONData,
+  writeLocalJSONData,
+} from './json.service';
+import {Alert} from 'react-native';
 import RNFS from 'react-native-fs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {unzipFile} from './unzip.service';
 
-const getJSONFromFirebase = async (path: string) => {
-  try {
-    const storageRef = storage().ref(path);
-    const url = await storageRef.getDownloadURL();
-    const json = await fetch(url).then(response => response.json());
+let startTime: number;
 
-    return json;
-  } catch (error) {
-    console.error('Error getting JSON file from Firebase', error);
-    return [];
+async function updateLearningData(
+  remoteDirPath: string,
+  localDirPath: string,
+  category: number,
+): Promise<boolean> {
+  const confirmed = await new Promise<boolean>(resolve => {
+    Alert.alert(
+      '',
+      '학습 데이터를 다운로드하시겠습니까? (통신 요금이 발생할 수 있으므로 Wi-Fi 환경에서 다운로드하는 것을 권장합니다.)',
+      [
+        {
+          text: '아니요',
+          style: 'cancel',
+          onPress: () => resolve(false),
+        },
+        {
+          text: '네',
+          onPress: () => resolve(true),
+        },
+      ],
+      {cancelable: false},
+    );
+  });
+
+  if (!confirmed) {
+    return false;
   }
-};
 
-export async function checkAndUpdateJSON() {
-  const remoteJSONPaths = ['data.json', 'learning/data.json'];
-  const data = {category: null, chapter: null};
+  if (!(await RNFS.exists(localDirPath))) {
+    await RNFS.mkdir(localDirPath);
+  }
 
-  for (const remoteJSONPath of remoteJSONPaths) {
-    const remoteDirPath = remoteJSONPath.substring(
-      0,
-      remoteJSONPath.lastIndexOf('/'),
+  startTime = Date.now();
+
+  await unzipFile(
+    `${remoteDirPath}/I_${category}.zip`,
+    `${localDirPath}/images.zip`,
+  );
+  await unzipFile(
+    `${remoteDirPath}/S_${category}.zip`,
+    `${localDirPath}/sounds.zip`,
+  );
+
+  let elapsedTime = Date.now() - startTime;
+  console.log(
+    `elapsed time: ${Math.floor(elapsedTime / 1000)}s ${elapsedTime % 1000}ms`,
+  );
+
+  Alert.alert(
+    '',
+    '모든 데이터가 성공적으로 다운로드되었습니다. 학습을 진행하세요!',
+    [{text: "Let's go!"}],
+  );
+
+  return true;
+}
+
+export async function checkAndUpdateLearningData(
+  category: number,
+  navigation: any,
+) {
+  const remoteJSONPath = `learning/${category}/${category}.json`;
+  const remoteDirPath = remoteJSONPath.slice(
+    0,
+    remoteJSONPath.lastIndexOf('/'),
+  );
+
+  const localJSONPath = `${RNFS.DocumentDirectoryPath}/${remoteJSONPath}`;
+  const localDirPath = `${RNFS.DocumentDirectoryPath}/${remoteDirPath}`;
+
+  try {
+    const {localVersion, remoteVersion, remoteJson} = await checkJSONVersion(
+      localJSONPath,
+      remoteJSONPath,
     );
 
+    if (localVersion !== remoteVersion) {
+      const isSucceeded = await updateLearningData(
+        remoteDirPath,
+        localDirPath,
+        category,
+      );
+
+      if (isSucceeded) {
+        await writeLocalJSONData(localJSONPath, remoteJson);
+        return remoteJson.data;
+      } else {
+        navigation.goBack();
+        return null;
+      }
+    } else {
+      return (await readLocalJSONData(localJSONPath)).data;
+    }
+  } catch (error) {
+    console.error('Error handling learning JSON data update: ', error);
+    navigation.goBack();
+    return null;
+  }
+}
+
+export async function checkAndUpdateGeneralData() {
+  const remoteJSONPaths = ['data.json', 'learning/data.json'];
+
+  for (const remoteJSONPath of remoteJSONPaths) {
     const localJSONPath = `${RNFS.DocumentDirectoryPath}/${remoteJSONPath}`;
-    const localDirPath = `${RNFS.DocumentDirectoryPath}/${remoteDirPath}`;
 
     try {
-      let localVersion: string | null = null;
-
-      if (await RNFS.exists(localJSONPath)) {
-        const localJSON = JSON.parse(
-          await RNFS.readFile(localJSONPath, 'utf8'),
-        );
-        localVersion = localJSON.version;
-      }
-
-      const remoteJSON = await getJSONFromFirebase(remoteJSONPath);
-      if (!remoteJSON) {
-        throw new Error(`Failed to fetch remote data from ${remoteJSONPath}`);
-      }
-
-      const remoteVersion = remoteJSON.version;
+      const {localVersion, remoteVersion, remoteJson} = await checkJSONVersion(
+        localJSONPath,
+        remoteJSONPath,
+      );
 
       if (localVersion !== remoteVersion) {
-        if (!(await RNFS.exists(localDirPath))) {
-          await RNFS.mkdir(localDirPath);
+        await writeLocalJSONData(localJSONPath, remoteJson);
+
+        if (remoteJSONPath === 'data.json') {
+          await AsyncStorage.setItem(
+            'categories',
+            JSON.stringify(remoteJson.data),
+          );
+        } else {
+          await AsyncStorage.setItem(
+            'chapters',
+            JSON.stringify(remoteJson.data),
+          );
         }
-        await RNFS.writeFile(
-          localJSONPath,
-          JSON.stringify(remoteJSON, null, 2),
-          'utf8',
-        );
-        remoteJSONPath === 'data.json'
-          ? await AsyncStorage.setItem(
-              'categories',
-              JSON.stringify(remoteJSON.data),
-            )
-          : await AsyncStorage.setItem(
-              'chapters',
-              JSON.stringify(remoteJSON.data),
-            );
       }
     } catch (error) {
-      console.error(`Error handling JSON file at ${remoteJSONPath}:`, error);
+      console.error(
+        `Error handling general JSON file at ${remoteJSONPath}:`,
+        error,
+      );
     }
   }
 
-  return data;
+  return null; // Return type could be adjusted based on specific requirements
 }
--- front/src/services/json.service.ts
+++ front/src/services/json.service.ts
@@ -1,99 +1,127 @@
 import storage from '@react-native-firebase/storage';
 import RNFS from 'react-native-fs';
-import {Alert} from 'react-native';
-import {unzipFile} from './unzip.service';
 
-let startTime;
-
-export async function checkAndUpdateJSON(category: number, navigation: any) {
-  const remoteJSONPath = `learning/${category}/${category}.json`;
-  const remoteDirPath = remoteJSONPath.slice(
-    0,
-    remoteJSONPath.lastIndexOf('/'),
-  );
-
-  const localJSONPath = `${RNFS.DocumentDirectoryPath}/${remoteJSONPath}`;
-  const localDirPath = `${RNFS.DocumentDirectoryPath}/${remoteDirPath}`;
-
-  try {
-    let localVersion: string | null = null;
-
-    if (await RNFS.exists(localJSONPath)) {
-      const localData = JSON.parse(await RNFS.readFile(localJSONPath, 'utf8'));
-      localVersion = localData.version;
-    }
-
-    const storageRef = storage().ref(remoteJSONPath);
-    const url = await storageRef.getDownloadURL();
-    const remoteJson = await fetch(url).then(response => response.json());
-    const remoteVersion = remoteJson.version;
-
-    if (localVersion !== remoteVersion) {
-      const confirmed = await new Promise<boolean>(resolve => {
-        Alert.alert(
-          '',
-          '학습 데이터를 다운로드하시겠습니까? (통신 요금이 발생할 수 있으므로 Wi-Fi 환경에서 다운로드하는 것을 권장합니다.)',
-          [
-            {
-              text: '아니요',
-              style: 'cancel',
-              onPress: () => resolve(false),
-            },
-            {
-              text: '네',
-              onPress: () => resolve(true),
-            },
-          ],
-          {cancelable: false},
-        );
-      });
-
-      // '아니오'를 누르는 경우
-      if (!confirmed) {
-        navigation.goBack();
-        return;
-      }
-
-      // '예'를 누르는 경우
-      if (!(await RNFS.exists(localDirPath))) {
-        await RNFS.mkdir(localDirPath);
-      }
-      await RNFS.writeFile(
-        localJSONPath,
-        JSON.stringify(remoteJson, null, 2),
-        'utf8',
-      );
-
-      startTime = Date.now();
-
-      await unzipFile(
-        `${remoteDirPath}/I_${category}.zip`,
-        `${localDirPath}/images.zip`,
-      );
-      await unzipFile(
-        `${remoteDirPath}/S_${category}.zip`,
-        `${localDirPath}/sounds.zip`,
-      );
-
-      let elapsedTime = Date.now() - startTime;
-      console.log(
-        `elapsed time: ${Math.floor(elapsedTime / 1000)}s ${
-          elapsedTime % 1000
-        }ms`,
-      );
-
-      Alert.alert(
-        '',
-        '모든 데이터가 성공적으로 다운로드되었습니다. 학습을 진행하세요!',
-        [{text: "Let's go!"}],
-      );
-
-      return remoteJson.data;
-    } else {
-      const localData = JSON.parse(await RNFS.readFile(localJSONPath, 'utf8'));
-      return localData.data;
-    }
-  } catch (error) {
-    console.error('Error handling file: ', error);
+export async function readLocalJSONData(localJSONPath: string) {
+  if (await RNFS.exists(localJSONPath)) {
+    const localData = JSON.parse(await RNFS.readFile(localJSONPath, 'utf8'));
+    return localData;
   }
+  return null;
 }
+
+export async function readRemoteJSONData(remoteJSONPath: string) {
+  const storageRef = storage().ref(remoteJSONPath);
+  const url = await storageRef.getDownloadURL();
+  const remoteJson = await fetch(url).then(response => response.json());
+  return remoteJson;
+}
+
+export async function writeLocalJSONData(localJSONPath: string, data: any) {
+  await RNFS.writeFile(localJSONPath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+export async function checkJSONVersion(
+  localJSONPath: string,
+  remoteJSONPath: string,
+) {
+  const localJSON = await readLocalJSONData(localJSONPath);
+  const localVersion = localJSON?.version || null;
+
+  const remoteJson = await readRemoteJSONData(remoteJSONPath);
+  const remoteVersion = remoteJson.version;
+
+  return {localVersion, remoteVersion, remoteJson};
+}
+
+// export async function checkAndUpdateJSON(category: number, navigation: any) {
+//   const remoteJSONPath = `learning/${category}/${category}.json`;
+//   const remoteDirPath = remoteJSONPath.slice(
+//     0,
+//     remoteJSONPath.lastIndexOf('/'),
+//   );
+
+//   const localJSONPath = `${RNFS.DocumentDirectoryPath}/${remoteJSONPath}`;
+//   const localDirPath = `${RNFS.DocumentDirectoryPath}/${remoteDirPath}`;
+
+//   try {
+//     let localVersion: string | null = null;
+
+//     if (await RNFS.exists(localJSONPath)) {
+//       const localData = JSON.parse(await RNFS.readFile(localJSONPath, 'utf8'));
+//       localVersion = localData.version;
+//     }
+
+//     const storageRef = storage().ref(remoteJSONPath);
+//     const url = await storageRef.getDownloadURL();
+//     const remoteJson = await fetch(url).then(response => response.json());
+//     const remoteVersion = remoteJson.version;
+
+//     if (localVersion !== remoteVersion) {
+//       const confirmed = await new Promise<boolean>(resolve => {
+//         Alert.alert(
+//           '',
+//           '학습 데이터를 다운로드하시겠습니까? (통신 요금이 발생할 수 있으므로 Wi-Fi 환경에서 다운로드하는 것을 권장합니다.)',
+//           [
+//             {
+//               text: '아니요',
+//               style: 'cancel',
+//               onPress: () => resolve(false),
+//             },
+//             {
+//               text: '네',
+//               onPress: () => resolve(true),
+//             },
+//           ],
+//           {cancelable: false},
+//         );
+//       });
+
+//       // '아니오'를 누르는 경우
+//       if (!confirmed) {
+//         navigation.goBack();
+//         return;
+//       }
+
+//       // '예'를 누르는 경우
+//       if (!(await RNFS.exists(localDirPath))) {
+//         await RNFS.mkdir(localDirPath);
+//       }
+//       await RNFS.writeFile(
+//         localJSONPath,
+//         JSON.stringify(remoteJson, null, 2),
+//         'utf8',
+//       );
+
+//       startTime = Date.now();
+
+//       await unzipFile(
+//         `${remoteDirPath}/I_${category}.zip`,
+//         `${localDirPath}/images.zip`,
+//       );
+//       await unzipFile(
+//         `${remoteDirPath}/S_${category}.zip`,
+//         `${localDirPath}/sounds.zip`,
+//       );
+
+//       let elapsedTime = Date.now() - startTime;
+//       console.log(
+//         `elapsed time: ${Math.floor(elapsedTime / 1000)}s ${
+//           elapsedTime % 1000
+//         }ms`,
+//       );
+
+//       Alert.alert(
+//         '',
+//         '모든 데이터가 성공적으로 다운로드되었습니다. 학습을 진행하세요!',
+//         [{text: "Let's go!"}],
+//       );
+
+//       return remoteJson.data;
+//     } else {
+//       const localData = JSON.parse(await RNFS.readFile(localJSONPath, 'utf8'));
+//       return localData.data;
+//     }
+//   } catch (error) {
+//     console.error('Error handling file: ', error);
+//   }
+// }
